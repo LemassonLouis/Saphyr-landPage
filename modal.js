5c56efbc63cde1922693f7075e4f0f338b0e42e2
LEMASSON Louis <galou2608@gmail.com>
2022-12-09 09:49:23 +0100

    Clean repo

--- modal.js
+++
@@ -1,9 +0,0 @@
-function openModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "flex";
-}
-
-function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-}
